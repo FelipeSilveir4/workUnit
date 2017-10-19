e3af4f7e229e184ed87892150c9e559cdea3787e
Felipe Silveira <felipesilveira.dev@gmail.com>
2017-10-19 16:32:05 -0200

    Addin support to the machine learning application

--- src/main/java/silveira/felipe/workunit/work/union/WorkerDispatcher.java
+++ src/main/java/silveira/felipe/workunit/work/union/WorkerDispatcher.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2017.  Felipe Silveira
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,7 +33,6 @@ import silveira.felipe.workunit.model.WorkReport;
 import silveira.felipe.workunit.model.WorkerThread;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -48,7 +47,7 @@ public class WorkerDispatcher {
     /**
      * Max number of workers.
      */
-    private static final int MAX_WORKERS = 10;
+    private static final int MAX_WORKERS = Integer.parseInt(System.getenv("MAX_WORKERS"));
 
     /**
      * Static reference to a WorkerDispatcher instance.
